366c6ff58b3bbdc132b741ff079d88fff8f891aa
robegelmeer <38240619+robegelmeer@users.noreply.github.com>
2021-11-18 13:00:25 +0100

    make two seperate files for updating the statistics

--- update_user_statistics/update_user_statistics_all.ps1
+++ update_user_statistics/update_user_statistics_all.ps1
@@ -3,5 +3,5 @@
 #geef het kaartviewer URL
 $kaartviewerURL=""
 
-#statistieken van vorige dag
-Invoke-WebRequest -Uri $kaartviewerURL/admin/rest/auth/log/convert
+#alle statistieken
+Invoke-WebRequest -Uri $kaartviewerURL/admin/rest/auth/log/convert/all
