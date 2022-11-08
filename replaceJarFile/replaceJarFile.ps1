cdc48997a7cf947d0869dd7c9a6752f3b6576421
robegelmeer <38240619+robegelmeer@users.noreply.github.com>
2022-11-08 17:09:45 +0100

    fixes after test

--- replaceJarFile/replaceJarFile.ps1
+++ replaceJarFile/replaceJarFile.ps1
@@ -5,23 +5,24 @@ $newJarFilename = "commons-text-1.10.jar"
 $queryString = "name='" + $mapFishService.Name + "'"
 
 $mapfishInstanceExe = Get-CimInstance win32_service -filter $queryString
+
 if (Test-path $mapfishInstanceExe.PathName.split()[0])
 {
     #pad naar mapfishInstanceExe bestaat
     $mapfishInstanceExePath = Get-Item -Path $mapfishInstanceExe.PathName.split()[0]
 
-    #deze regel verwijderen!
-    $mapfishInstanceExePath = Get-Item -Path "D:\\Program Data\\apache-tomcat\\mapfish"
+    $libPath = Join-Path -Path $mapfishInstanceExePath.Directory.Parent.FullName -ChildPath webapps\print\WEB-INF\lib
     
-    $libPath = Join-Path -Path $mapfishInstanceExePath.FullName -ChildPath webapps\print\WEB-INF\lib
     $oldJarFile = Join-Path -Path $libPath -ChildPath $jarFileNameToReplace
-
     if (Test-path $oldJarFile) {
         Stop-Service $mapFishService.Name
 
         Rename-Item -Path $oldJarFile -newName "_commons-text-1.6.jar"
-        $newJarFilename = Get-ChildItem -Path $PSScriptRoot -Filter $newJarFilename
-        $renamedJarFile = Rename-Item -Path $newJarFilename[0].Name -newName $jarFileNameToReplace -PassThru
+        
+        $newJarFilename = Get-ChildItem -Path $PSScriptRoot -Name $newJarFilename
+        Write-Output $newJarFilename
+
+        $renamedJarFile = Rename-Item -Path $newJarFilename -newName $jarFileNameToReplace -PassThru
 
         Copy-item -Path $renamedJarFile -Destination $oldJarFile -Force
 
