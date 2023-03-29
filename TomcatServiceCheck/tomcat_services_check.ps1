b6f0d00ffcc96cd0fdc638bdccf7707ce3d2e2f3
Rob Egelmeer <rob@geonovation.nl>
2023-03-29 15:32:55 +0200

    Added tomcat services check

---
+++ TomcatServiceCheck/tomcat_services_check.ps1
@@ -0,0 +1,12 @@
+﻿#get all tomcat services
+$tomcatServices = Get-Service tomcat*
+
+foreach ($tomcatService in $tomcatServices)
+{
+   
+    if ($tomcatService.Status -ne "Running") 
+    {
+        Start-Service $tomcatServices.Name
+        $tomcatService.WaitForStatus("Running")
+    }
+}
