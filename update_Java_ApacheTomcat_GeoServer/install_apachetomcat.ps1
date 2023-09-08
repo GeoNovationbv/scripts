2121767dacfd1a9eb4f1e8c542cc7d7fe0e6d7c5
Rob Egelmeer <rob@geonovation.nl>
2023-09-08 07:00:35 +0200

    updates of script for setting correct geoserver data dir

--- update_Java_ApacheTomcat_GeoServer/install_apachetomcat.ps1
+++ update_Java_ApacheTomcat_GeoServer/install_apachetomcat.ps1
@@ -45,26 +45,29 @@ $apacheTomcatDir = Get-ChildItem -Path $extractedApacheTomcatDirPath -Directory
 #sets CATALINA_HOME folder
 $CATALINA_HOME = join-path -Path $installLocationFileConfig["appDir"] -ChildPath $extractedApacheTomcatDir
 $env:CATALINA_HOME = $CATALINA_HOME
+$reinstallServices = $false
 
 if (-not (Test-Path -Path $CATALINA_HOME) ) {
     #create the new CATALINA_HOME folder
     New-Item $CATALINA_HOME -ItemType "directory"
-}
-
-#copies Apache Tomcat bin en lib folder 
-foreach ($apacheTomcatBinLibDir in Get-ChildItem -Path $extractedApacheTomcatDirPath -Directory )
-{
-    if ($apacheTomcatBinLibDir.BaseName -eq "bin" -Or $apacheTomcatBinLibDir.BaseName -eq "lib") {
+    
+    #copies Apache Tomcat bin en lib folder 
+    foreach ($apacheTomcatBinLibDir in Get-ChildItem -Path $extractedApacheTomcatDirPath -Directory )
+    {
+        if ($apacheTomcatBinLibDir.BaseName -eq "bin" -Or $apacheTomcatBinLibDir.BaseName -eq "lib") {
             
-        Copy-Item -path $apacheTomcatBinLibDir.FullName -destination $CATALINA_HOME -Recurse -Force
+            Copy-Item -path $apacheTomcatBinLibDir.FullName -destination $CATALINA_HOME -Recurse -Force
+        }
     }
+
+    #copies Apache Tomcat root files 
+    foreach ($apacheTomcatFile in Get-ChildItem -Path $extractedApacheTomcatDirPath -File) {
+        Copy-Item $apacheTomcatFile.FullName $CATALINA_HOME -Force 
+    }
+
+    $reinstallServices = $true
 }
 
-#copies Apache Tomcat files 
-foreach ($apacheTomcatFile in Get-ChildItem -Path $extractedApacheTomcatDirPath -File) {
-    Copy-Item $apacheTomcatFile.FullName $CATALINA_HOME -Force 
-}
-  
 #Lees de installatie bestanden uit
 Foreach ($updateConfigFile in $updateConfigFiles)
 {
@@ -76,12 +79,31 @@ Foreach ($updateConfigFile in $updateConfigFiles)
     # stop the current Apache Tomcat service
     $oldApacheTomcatServiceNameFilter = "tomcat*"+$config["serviceName"]
     $oldApacheTomcatService = Get-Service -Include $oldApacheTomcatServiceNameFilter
+    $oldApacheTomcatServiceName = $oldApacheTomcatService.Name
 
     if(-Not ($oldApacheTomcatService -eq $null) ) {
+        $ApachRunAsOtherUserMessage = ""
+
         if ($oldApacheTomcatService.Status -eq "Running") 
         {
-            stop-service -name $oldApacheTomcatService.Name
+            stop-service -name $oldApacheTomcatService.Name -Verbose:$false
             $oldApacheTomcatService.WaitForStatus('Stopped','00:00:45')
+        }
+
+        #controleer het account wat de Apache Tomcat service uitvoert.
+        $oldApcheTomcatServiceRunAs = (Get-WmiObject Win32_Service -Filter "Name='$oldApacheTomcatService.Name'").StartName
+        if ( $oldApcheTomcatServiceRunAs -ne $null ) {
+            $ApachRunAsOtherUserMessage = "Oude Apache Tomcat service met naam: " + $oldApacheTomcatServiceName + " wordt uitgevoerd door account: " + $oldApcheTomcatServiceRunAs
+        }
+        else {
+            $ApachRunAsOtherUserMessage = "Oude Apache Tomcat service met naam: " + $oldApacheTomcatServiceName + " wordt uitgevoerd door een system account"
+        }
+        Write-Output $ApachRunAsOtherUserMessage
+    }
+    else {
+        if (-not $reinstallServices) {
+            #apache tomcat folder exists but service is not installed install the service
+            $reinstallServices = $true
         }
     }
 
@@ -146,25 +168,43 @@ Foreach ($updateConfigFile in $updateConfigFiles)
     #renames the war file
     Set-Location $apacheTomcatWebAppsDir
     $newWarFilename =  $config["ApplicationName"]+".war"
+
     Rename-Item -path $warFile.Name -NewName $newWarFilename -Force
 
-    #install Apache Tomcat service
     $CATALINA_HOME_bin = Join-Path -Path $CATALINA_HOME -ChildPath "bin"
     Set-Location $CATALINA_HOME_bin
-
+        
+    #set the new service name
     $apacheTomcatServiceName = "tomcat" + $apacheTomcatVersion + "-" + $config["serviceName"]
 
-    #install Apache Tomcat service
-    Start-Process .\service.bat -ArgumentList install,$apacheTomcatServiceName -Wait
-        
+    if ($reinstallServices) {
+        #install Apache Tomcat service
+        if ( $oldApacheTomcatService -ne $null -and $oldApcheTomcatServiceRunAs -eq $null ) {
+            $oldApacheTomcatServiceRemoved = "Oude Apache Tomcat service met naam: " + $oldApacheTomcatServiceName + " wordt verwijderd."
+            Start-Process .\service.bat -ArgumentList remove, $oldApacheTomcatService.Name -Wait
+        }
+        #install Apache Tomcat service
+        Start-Process .\service.bat -ArgumentList install,$apacheTomcatServiceName -Wait
+    }
+    
+    if ($config["MapFishLoggingConfig"] -ne $null) {
+        $logBackLoggingLocation = $config["MapFishLoggingConfig"]
+        if (-not (Test-Path -Path $logBackLoggingLocation) ) {
+            New-Item -ItemType File $logBackLoggingLocation -Force
+            $currentDirectoryinstallatie_templates = Join-Path -Path $currentDirectory -ChildPath "installatie_templates"
+            Copy-Item $currentDirectoryinstallatie_templates"\mapfish\logback.xml" $logBackLoggingLocation -Force
+        }
+        $jvmOptionLoggingConfig = ";-Dlogback.configurationFile=$logBackLoggingLocation"
+        $config["JvmOptions"] = $config["JvmOptions"]+$jvmOptionLoggingConfig
+    }
+
     #update the services with the settings
     $JvmMs = $config["JvmMs"]
     $JvmMx = $config["JvmMx"]
     $jvmOptions = $config["JvmOptions"]
     Start-Process .\tomcat9.exe -ArgumentList //US/$apacheTomcatServiceName,--Startup=auto,--Jvm=auto,--LogLevel=error,--JvmMs=$JvmMs,--JvmMx=$JvmMx,++JvmOptions=$jvmOptions -Wait
-        
+
     $apacheTomcatService = Get-Service -Name $apacheTomcatServiceName
-    
 
     Start-Service $apacheTomcatServiceName
     $apacheTomcatService.WaitForStatus('Running')
@@ -180,7 +220,9 @@ Foreach ($updateConfigFile in $updateConfigFiles)
     Remove-Item $newWarFilename
 
     $apacheTomcatInstalledWebAppsDir = Join-Path $apacheTomcatWebAppsDir -ChildPath $config["ApplicationName"] 
-    $empty_geoserver_data_dir = Get-ChildItem -Path $apacheTomcatInstalledWebAppsDir -Filter data -Directory
+    Write-Output $apacheTomcatInstalledWebAppsDir
+    $empty_geoserver_data_dir = Get-ChildItem -Path $apacheTomcatInstalledWebAppsDir -Filter data 
+    Write-Output $empty_geoserver_data_dir
 
     if ($config["Geoserver_extensions"]) 
     {
@@ -199,31 +241,28 @@ Foreach ($updateConfigFile in $updateConfigFiles)
             Copy-Item -Path $geoserverExtensionExtractedDir\*.jar -Destination $geoserverWEBINFlibDir -Force
         }
     }
-    Write-Output $empty_geoserver_data_dir.FullName
-    if ($empty_geoserver_data_dir -And $config["NewGeoserverDataDir"] ) 
+
+    #we moeten iets gaan doen met de geoserver data dir
+    
+    if ($config["GeoserverDataDir"] ) 
     {
-        #dit is niet helemaal logisch
-        #mogelijke scenario's : nieuwe data dir uit geoserver, bestaande data dir blijven gebruiken of kopie van oude omgeving.
-        if (-Not (Test-Path $config["NewGeoserverDataDir"]) ){
-            New-Item -Path $config["NewGeoserverDataDir"] -ItemType "directory"
-        }
+        if (-Not (Test-Path $config["GeoserverDataDir"]) ){
+            New-Item -Path $config["GeoserverDataDir"] -ItemType "directory"
 
-        #copies the geoserver data dir to the new location.
-        if (-Not ($config["OldGeoserverDataDir"]) )
-        {
             #moves the empty_geoserver_data_dir to the new location
-            Get-ChildItem -Path $empty_geoserver_data_dir.FullName -Recurse | Move-Item -Destination $config["NewGeoserverDataDir"] -Force
+            Get-ChildItem -Path $empty_geoserver_data_dir.FullName -Recurse | Copy-Item -Destination $config["NewGeoserverDataDir"] -Force
         }
-        else
-        {
-            Remove-Item -path $geoserver_empty_data_dir.FullName -Recurse
-            Get-Acl $config["OldGeoserverDataDir"] | Set-Acl $config["NewGeoserverDataDir"]
-
-            #copy the empty_geoserver_data_dir to the new location
-            Copy-Item -Path $config["OldGeoserverDataDir"]\*.* -Destination $config["NewGeoserverDataDir"] -Recurse
+        else {
+            #backup geoserver_data_dir
+            $currentDate = Get-Date -Format "yyyy-MM-dd"
+            $GeoServerDataDirBackupFolder = $config["GeoserverDataDir"] + "_backup_" + $currentDate 
+            #Copy-Item -Path (Get-Item -Path $config["GeoserverDataDir"] + "\*+" -Exclude ("data")).FullName -Destination $GeoServerDataDirBackupFolder -Force -Recurse
+            Copy-Item -Path $config["GeoserverDataDir"] -Destination $GeoServerDataDirBackupFolder -Force -Recurse
         }
 
-        $newGeoserverDataDir =  $config["NewGeoserverDataDir"]
+        Remove-Item -path $empty_geoserver_data_dir.FullName -Recurse
+
+        $newGeoserverDataDir =  $config["GeoserverDataDir"]
 
         Set-Location $CATALINA_HOME_bin
         
@@ -232,12 +271,15 @@ Foreach ($updateConfigFile in $updateConfigFiles)
         Start-Process .\tomcat9.exe -ArgumentList //US/$apacheTomcatServiceName,++JvmOptions=$dgeoserver_data_dir -Wait
     }
     
-    #Start-Service $apacheTomcatServiceName
+    if ( $oldApcheTomcatServiceRunAs -eq $null ) {
+        Start-Service $apacheTomcatServiceName
+    }
+    else {
+        Write-Output $apacheTomcatServiceName " nog niet gestart moet worden uitgevoerd door windows account: " + $oldApcheTomcatServiceRunAs
+    }
+
     Set-Location -Path $currentDirectory
     
 }
 
 
-#uninstall the old Apache Tomcat service
-#Set-Location $CATALINA_HOME_bin
-#Start-Process .\service.bat -ArgumentList remove, $oldApacheTomcatService.Name -Wait
