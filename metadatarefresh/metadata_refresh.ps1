e1f56ba9504208639ea6ff0c8f1157e397059215
admin-lges01 <admin-lges01@hgh001.gegevenshuis.nl>
2024-01-24 14:55:21 +0100

    first version of metarefresh script

---
+++ metadatarefresh/metadata_refresh.ps1
@@ -0,0 +1,7 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
+
+#geef het metadata refresh URL
+$simplesaml_metadatarefresh = "https://testgeo.gegevenshuis.nl/simplesaml/module.php/cron/run/daily/5euJxIl5jZ7GYOhaMLJg3SpkbHb2d1fy"
+
+#Metadata refresh
+Invoke-WebRequest -Uri $simplesaml_metadatarefresh
